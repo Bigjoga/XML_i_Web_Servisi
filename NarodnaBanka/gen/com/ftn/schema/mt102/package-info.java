df2b153c1e9c78823c7fdb5d8c28a7c4bd873365
Bigjoga <gajicnikola41@gmail.com>
2018-02-09 15:39:08 +0100

    Dodate seme i wsdl u Narodnoj Banci, i nakon toga izgenersane klase Narodne Banke.

---
+++ NarodnaBanka/gen/com/ftn/schema/mt102/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.com/schema/mt102", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.ftn.schema.mt102;
---
+++ NarodnaBanka/gen/com/ftn/schema/mt910/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.com/schema/mt910", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.ftn.schema.mt910;
