998f33920258b735677786bad011946e7fe20326
Bigjoga <gajicnikola41@gmail.com>
2018-02-09 17:53:37 +0100

    Napravljen model za Frimu.

---
+++ Firma/src/main/java/com/ftn/Frima/app/models/FrimaModel.java
@@ -0,0 +1,71 @@
+package com.ftn.Frima.app.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Firma")
+public class FrimaModel implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "naziv")
+	private String naziv;
+	
+	@Column(name = "adresa")
+	private String adresa;
+	
+	@Column(name = "pib")
+	private String pib;
+
+	public FrimaModel() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+	public String getPib() {
+		return pib;
+	}
+
+	public void setPib(String pib) {
+		this.pib = pib;
+	}
+	
+}
